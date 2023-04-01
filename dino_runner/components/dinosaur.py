57ee3d71a3e786ac3f02b75a91ae063eb55925a9
Juliana Perez <julianaperez10987@gmail.com>
2023-03-31 20:25:13 -0500

    Tarea Clase 2

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,13 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosour(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5
+    DUCK_SPEED = 350
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -16,19 +17,35 @@ class Dinosour(Sprite):
         self.step_index = 0
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED
+        self.dino_duck = False
+        self.duck_speed = self.DUCK_SPEED
 
     def update(self,user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_duck= False
+            self.dino_run = True 
+        
+        if user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_duck= False
             self.dino_run = True
+
 
         if self.step_index > 10:
             self.step_index = 0
@@ -52,4 +69,12 @@ class Dinosour(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.DUCK_SPEED
+        self.step_index += 1
+
 
